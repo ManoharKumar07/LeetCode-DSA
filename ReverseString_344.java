c6f1bdad0cb2912ffaf7202698d32b9e51057e56
Manohar Kumar <manohar.csengineer@gmail.com>
2024-05-16 21:51:44 +0530

    3rd problem

--- ReverseString_344.java
+++ ReverseString_344.java
@@ -1,14 +1,41 @@
-class Solution {
-    public void reverseString(char[] s) {
+import java.util.Scanner;
+
+public class ReverseString_344{
+
+    public static void reversestring(char [] s){
+
+        char temparr []=new char[s.length];
         int i=0;
-        char rev[]=new char[s.length];
         for(int j=s.length-1;j>=0;j--){
-            rev[i]=s[j];
+            temparr[i]=s[j];
             i++;
         }
+     for(int k=0;k<temparr.length;k++){
+        s[k]=temparr[k];
+     }
 
-        for(int j=0;j<rev.length;j++){
-            s[j]=rev[j];
-        }
     }
+
+public static void main (String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the string to reverse:");
+String word=sc.nextLine();
+
+char [] s= new char[word.length()];
+for (int i=0;i<word.length();i++){
+    s[i]=word.charAt(i);
+}
+for(char c:s){
+    System.out.println(c);
+}
+reversestring(s);
+
+System.out.println("Reversed String:");
+String reversedstring =new String(s);
+System.out.println(reversedstring);
+
+
+
+}
+
 }
