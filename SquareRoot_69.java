2e0b34f273fe47a105463de5eaeb0fa06bc412b7
Manohar Kumar <manohar.csengineer@gmail.com>
2024-05-18 23:51:43 +0530

    commit

---
+++ SquareRoot_69.java
@@ -0,0 +1,22 @@
+public class SquareRoot_69{
+    public static int mySqrt(int x) {
+  
+        int low=1,high=x,mid;
+        mid=low+high/2;
+        while(low<=high){
+            mid=low+high/2;
+            if(mid*mid<=x){
+                low=mid+1;
+            }
+            else{
+            high=mid-1;
+            }
+        }
+        return high;
+    }
+ public static void main(String[] args) {
+     int sq = mySqrt(10);
+     System.out.println(sq);
+ }
+    
+}
