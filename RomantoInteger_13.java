0e73091d6ed330a6a80f4db9ed508b992769a019
Manohar Kumar <manohar.csengineer@gmail.com>
2024-05-28 23:46:31 +0530

    problem 8

--- RomantoInteger_13.java
+++ RomantoInteger_13.java
@@ -1,35 +1,72 @@
+import java.util.*;
+
 public class RomantoInteger_13 {
    
+ 
 
-class Solution {
-    public int romanToInt(String s) {
+   
+        public static int romanToInt(String s) {
+    
+            Map <String,Integer> map = new HashMap<>();
+            map.put("I",1);
+            map.put("V",5);
+            map.put("X",10);
+            map.put("L",50);
+            map.put("C",100);
+            map.put("D",500);
+            map.put("M",1000);
+            
+            int num = 0;
+            int prevVal = 0;
+    
+            for(int i = s.length() - 1; i >= 0; i--) {
+                int curVal = map.get(String.valueOf(s.charAt(i)));
+                if(curVal < prevVal) {
+                    num -= curVal;
+                      prevVal = curVal;
+                } else {
+                    num += curVal;
+                      prevVal = curVal;
+                }
+              
+            }
+            
+            return num;
+        }
+    
+    public static void main(String[] args) {
+        System.out.println(romanToInt("XV"));
+    }
+}
+// class Solution {
+//     public int romanToInt(String s) {
 
-        Map <String,Integer> map = new HashMap<>();
-        map.put("I",1);
-        map.put("V",5);
-        map.put("X",10);
-        map.put("L",50);
-        map.put("C",100);
-        map.put("D",500);
-        map.put("M",1000);
- int num=0;
- int preval=0;
- int curval=0;
- for(int i=s.length()-1;i>=0;i++){
-    curval=map.get(String.valueOf(s.charAt(i)));
-    if(curval<preval){
-        num=num-preval;
-        preval=curval;
-    }
-    else if(curval>preval){
-        num=num+curval;
-preval=curval;
-    }
-    else{
-        preval=preval+curval;
-    }
+//         Map <String,Integer> map = new HashMap<>();
+//         map.put("I",1);
+//         map.put("V",5);
+//         map.put("X",10);
+//         map.put("L",50);
+//         map.put("C",100);
+//         map.put("D",500);
+//         map.put("M",1000);
+//  int num=0;
+//  int preval=0;
+//  int curval=0;
+//  for(int i=s.length()-1;i>=0;i++){
+//     curval=map.get(String.valueOf(s.charAt(i)));
+//     if(curval<preval){
+//         num=num-preval;
+//         preval=curval;
+//     }
+//     else if(curval>preval){
+//         num=num+curval;
+// preval=curval;
+//     }
+//     else{
+//         preval=preval+curval;
+//     }
 
- }
+//  }
 
 
 
@@ -49,7 +86,3 @@ preval=curval;
         //     }
         // }
 
-        return num;
-    }
-}
-}
